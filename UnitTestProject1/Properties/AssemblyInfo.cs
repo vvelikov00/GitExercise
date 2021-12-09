50513558c61288cfae7bcd18d8ab7d11095e367c
Viktor Velikov <v.velikov00@abv.bg>
2021-12-09 13:36:44 +0200

    Create unit test

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("811e4644-3343-4bfe-825d-d9ca2e77ed3f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var math = new GitUnitTestDemo.Math(5, 2);
+            var math2 = new GitUnitTestDemo.Math(1234, 0);
+            var math3 = new GitUnitTestDemo.Math();
+
+            Assert.AreEqual(7, math.Add());
+            Assert.AreEqual(3, math.Subtract());
+            Assert.AreEqual(10, math.Multiply());
+            Assert.AreEqual(2.5, math.Divide());
+
+            Assert.IsTrue(math.Divide() > 2.49 && math.Divide() < 2.51);
+            Assert.AreEqual(Single.PositiveInfinity, math2.Divide());
+
+            Assert.AreEqual(10, math2.SumDigits());
+            Assert.AreEqual("101", math.Binary());
+
+            Assert.IsTrue(math.a == 5 && math.b == 2);
+            Assert.IsTrue(math3.a == 1 && math3.b == 1);
+
+        }
+    }
+}
